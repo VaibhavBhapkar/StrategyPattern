47e7e8f202612a28ec26aa5602d4139d2b1c3927
Vaibhav Bhapkar <vaibhavrb999@gmail.com>
2021-03-28 18:15:58 +0530

    Chnages after merge

--- StrategyPattern/ConfigurationSourceFactory.cs
+++ StrategyPattern/ConfigurationSourceFactory.cs
@@ -12,30 +12,6 @@ namespace StrategyPattern
             Type typeAssembly = FindTypeInAssembly.FindInstanceOfAssembly(source,typeof(IConfiguartionReader).ToString());
             if (typeAssembly == null) throw new Exception("Bad Type");
             else return Activator.CreateInstance(typeAssembly) as IConfiguartionReader;
-        }
-        private Type GetTypeToCreate(string sourceName)
-        {
-            foreach (var source in configSource)
-            {
-                Console.WriteLine(source.Key);
-                if (source.Key.Contains(sourceName))
-                {
-                    return configSource[source.Key];
-                }
-            }
-            return null;
-        }
-        private void LoadTypesAssemblyCanReturn()
-        {
-            configSource = new Dictionary<string, Type>();
-            Type[] typeInThisAssembly = Assembly.GetExecutingAssembly().GetTypes();
-            foreach (Type type in typeInThisAssembly)
-            {
-                if (type.GetInterface(typeof(IConfiguartionReader).ToString()) != null)
-                {
-                    configSource.Add(type.Name.ToUpper(), type);
-                }
-            }
-        }
+        }       
     }
 }
